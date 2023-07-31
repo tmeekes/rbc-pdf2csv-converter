5490d977eb52b3f650578ee6ba026159e2d8d5d8
Tony Meekes <tony@meekes.ca>
2023-07-31 15:50:46 -0400

    Need to remove everything after closing balance

--- pdf2csv.py
+++ pdf2csv.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np #Import numpy to handle NaN values
 import camelot
+import re
 import matplotlib.pyplot as plt
 from mysecrets import PDF_DIR, CSV_FILE
 
@@ -18,12 +19,10 @@ def get_pdf_files_recursive(PDF_DIR):
 headers_to_include = ["Date", "Description", "Withdrawals ($)", "Deposits ($)", "Balance ($)"]
 
 # Function to extract tables from the PDF using Camelot in stream mode
-def extract_tables_with_camelot(pdf_path, start_string="Details of your account activity"):
-    
+def extract_tables_with_camelot(pdf_path):
     # Read the first page separately, all others grouped in one loop afterwards
-#    columns=['42,88,317,423,553']
     tables_page1 = camelot.read_pdf(pdf_path, flavor='stream', pages='1', edge_tol=34)
-    tables_page2_onwards = camelot.read_pdf(pdf_path, flavor='stream', pages='2-end')
+    tables_page2_onwards = camelot.read_pdf(pdf_path, flavor='stream', pages='2-end', edge_tol=22)
     
     dataframes = []
 
@@ -31,50 +30,12 @@ def extract_tables_with_camelot(pdf_path, start_string="Details of your account 
         if table.df.empty:
             continue
 
-        df = table.df
-
-        # Find the index of the header row
-        header_index = df[df.apply(lambda row: all(header in " ".join(row) for header in headers_to_include), axis=1)].index
-
-        if len(header_index) > 0:
-            # If the header row is found, set the DataFrame to rows starting from that index
-            df = df.iloc[header_index[0] + 1:]
-        else:
-            # If the header row is not found, skip this table
-            continue
-
-        # Find the index of "Closing Balance" to remove rows after it
-        end_index = df[df.apply(lambda row: "Closing Balance" in " ".join(row), axis=1)].index
-
-        if len(end_index) > 0:
-            # If "Closing Balance" is found, set the DataFrame to rows until that index
-            df = df.iloc[:end_index[0] + 1]
-            # Drop the rows after "Closing Balance"
-            df = df.drop(index=df.index[end_index[0] + 1:])
-
-        # Drop the first column (index 0) from the DataFrame
-        df = df.drop(df.columns[0], axis=1)
-
-        # Append the DataFrame to the list
-        dataframes.append(df)
-    
-#    camelot.plot(tables_page1[0], kind='text')
-#plt.show(block=True)
-
-    for table in tables_page2_onwards:
-        if table.df.empty:
-            continue
-
-        # # Find the index of the start_string on other pages
-        # start_index = table.df[table.df.apply(lambda row: start_string in " ".join(row), axis=1)].index
-
-        # if len(start_index) > 0:
-        #     # If start_string is found, set the DataFrame to rows starting from that index
-        #     table.df = table.df.iloc[start_index[0] + 1:]
+        # Shift all columns left replace NaNs with blank strings (Removes the vertical left column string)
+        table.df = table.df.shift(periods=-1, axis=1)
+        table.df.fillna('', inplace=True)
 
         # Find the index of the header row
         header_index = table.df[table.df.apply(lambda row: all(header in " ".join(row) for header in headers_to_include), axis=1)].index
-        print(header_index)
 
         if len(header_index) > 0:
             # If the header row is found, set the DataFrame to rows starting from that index
@@ -92,13 +53,61 @@ def extract_tables_with_camelot(pdf_path, start_string="Details of your account 
             # Drop the rows after "Closing Balance"
             table.df = table.df.drop(index=table.df.index[end_index[0] + 1:])
 
+        # Drop the last column (index 5) from the DataFrame
+        table.df = table.df.drop(table.df.columns[5], axis=1)
+
         # Append the DataFrame to the list
         dataframes.append(table.df)
+
+        print(table.df)
+        print(f"Processed page 1")
+    
+    # camelot.plot(tables_page1[0], kind='text')
+    # plt.show(block=True)
+
+    for table in tables_page2_onwards:
+        if table.df.empty:
+            print(f"Page 2+ is empty")
+            continue
+
+        # Find the index of the header row
+        header_index = table.df[table.df.apply(lambda row: all(header in " ".join(row) for header in headers_to_include), axis=1)].index
+
+        if len(header_index) > 0:
+            # If the header row is found, set the DataFrame to rows starting from the row after that index
+            table.df = table.df.iloc[header_index[0] + 1:]
+        else:
+            # If the header row is not found, skip this table
+            continue
+
+        # Find the index of "Closing Balance" to remove rows from it and after
+        end_index = table.df[table.df.apply(lambda row: "Closing Balance" in " ".join(row), axis=1)].index
+
+        # Print whether "Closing Balance" was found on this page
+        print(f"'Closing Balance' found on page {table.page}: {len(end_index) > 0}")
+        print(end_index[0])
+
+        if len(end_index) > 0:
+            # If "Closing Balance" is found, set the DataFrame to rows until that index
+            table.df = table.df.iloc[:end_index[0]]
+            # Drop the rows from "Closing Balance"
+            table.df = table.df.drop(index=table.df.index[end_index[0]:])
+
+        print(end_index[0])
+
+        # Drop the last column (index 5) from the DataFrame
+        table.df = table.df.drop(table.df.columns[5], axis=1)
+        #print(table.df)
+
+        # Append the DataFrame to the list
+        dataframes.append(table.df)
+
+        print(f"Processed page 2+")
 
     return dataframes
 
 # Function to process PDFs and save data to CSV
-def process_pdfs(start_string=None):
+def process_pdfs():
     pdf_files = get_pdf_files_recursive(PDF_DIR)
     all_dataframes = []
 
@@ -115,12 +124,21 @@ def process_pdfs(start_string=None):
         # Concatenate all dataframes into a single DataFrame
         combined_data = pd.concat(all_dataframes, ignore_index=True)
         
-        # # Set the DataFrame columns using the headers from the first page
-        # combined_data.columns = headers_to_include
-        combined_data.columns = headers_to_include + ["Extra Date"]
+        # Set the DataFrame columns using the headers from the first page
+        combined_data.columns = headers_to_include
+        #combined_data.columns = headers_to_include + ["Extra Date"] # Use when the data is getting shifted from multiple PDFs
 
         # # Filter the DataFrame to keep only the required columns
         # combined_data = combined_data.filter(headers_to_include)
+
+        # # Print all dataframes in the list
+        # for i, df in enumerate(all_dataframes, 1):
+        #     print(f"\nDataframe {i}:")
+        #     print(df)
+        
+        # # Print the combined_data DataFrame
+        # print("\nCombined Data:")
+        # print(combined_data)
 
         csv_path = os.path.join(PDF_DIR, f"{CSV_FILE}.csv")
         combined_data.to_csv(csv_path, index=False)
