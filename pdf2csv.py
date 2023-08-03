109c4ffe276a9632394ed0b11e6eae19e3d69ccf
Tony Meekes <tony@meekes.ca>
2023-08-03 14:16:13 -0400

    +Error print log & Date/Descrip concat fix

--- pdf2csv.py
+++ pdf2csv.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np #Import numpy to handle NaN values
 import camelot
 import re
+import PyPDF2
 import matplotlib.pyplot as plt
 from mysecrets import PDF_DIR, CSV_FILE
 
@@ -18,8 +19,38 @@ def get_pdf_files_recursive(PDF_DIR):
 # Global variable to define headers to include
 headers_to_include = ["Date", "Description", "Withdrawals ($)", "Deposits ($)", "Balance ($)"]
 
+def pypdf2_extract_text_from_pdf(pdf_path, page_number):
+    with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        if page_number < 1 or page_number > pdf_reader.numPages:
+            raise ValueError(f"Invalid page number. Must be between 1 and {pdf_reader.numPages}.")
+
+        page = pdf_reader.getPage(page_number - 1)  # Page numbers are 0-based
+        return page.extract_text()
+
 # Function to extract tables from the PDF using Camelot in stream mode
 def extract_tables_with_camelot(pdf_path):
+    
+    # Use PyPDF2 to extract the text from the first page
+    pypdf2_text_extract = pypdf2_extract_text_from_pdf(pdf_path, page_number=1)
+
+    # Search for the pattern "Your RBC personal <anything> account statement"
+    match = re.search(r"Your\s+RBC\s+personal\s+.*\s+account\s+statement", pypdf2_text_extract, re.IGNORECASE)
+    # if not match:
+    #     return None
+
+    # Find the string right below the matched pattern
+    year_pattern = r"(\d{4})"
+    year_match = re.search(year_pattern, pypdf2_text_extract[match.end():])
+    # if not year_match:
+    #     return None
+
+    year = year_match.group(1)  # Extract the captured group containing the year
+
+    # Find the account number after "Your account number:"
+    account_number = re.search(r'Your account number:\s*(\d{5}-\d{7})', pypdf2_text_extract)
+    account_number = account_number.group(1) if account_number else None
+    
     # Read the first page separately, all others grouped in one loop afterwards
     tables_page1 = camelot.read_pdf(pdf_path, flavor='stream', pages='1', edge_tol=34)
     tables_page2_onwards = camelot.read_pdf(pdf_path, flavor='stream', pages='2-end', edge_tol=22)
@@ -40,7 +71,7 @@ def extract_tables_with_camelot(pdf_path):
             # If the header row is not found, skip this table
             continue
 
-        # If Date isn't the first column, shift all columns left replace NaNs with blank strings (Removes the vertical left column string)
+        # If Date isn't the first column, shift all columns left, replace NaNs with blank strings (Removes the vertical left column string)
         if ("Date" not in table.df.iloc[:, 0].values) and ("Date" in table.df.iloc[:, 1].values):
             table.df = table.df.shift(periods=-1, axis=1)
 
@@ -61,10 +92,33 @@ def extract_tables_with_camelot(pdf_path):
         if not end_index.empty:
             table.df = table.df.loc[:end_index.values[0] - 1]
 
+        #Append the year to the "Date" column for non-empty rows
+        #table.df[0] = [f"{date}, {year}" if date.strip() else date for date in table.df[0]]
+        table.df[0] = [f"{date}, {year}" if (date.strip() and date != "Date") else date for date in table.df[0]]
+        #table.df[0] = [f"{date}, {year}" if date.strip() and date != "Date" else date for date in table.df[0]]
+
+        # Insert new column for Account Numbers
+        table.df.insert(1, "Account Number", "")
+
+        # Append the account number to the "Account Number" column for non-empty rows
+        table.df["Account Number"] = [f"{account_number}" if description.strip() else description for description in table.df[1]]
+
+        # Loop through the DataFrame starting from the second row
+        for i in range(1, len(table.df)):
+            # Check if the description is not empty for the current row
+            if table.df.iloc[i, 2].strip():
+                # Check if both withdrawals and deposits are empty for the current row
+                if table.df.iloc[i, 3] == '' and table.df.iloc[i, 4] == '':
+                    # Concatenate the description with the next row's description
+                    table.df.iloc[i+1, 2] = table.df.iloc[i, 2] + ' | ' + table.df.iloc[i+1, 2]
+                    # Clear out the current row's description
+                    table.df.iloc[i, 2] = ''
+
+        # Drop rows where the description is empty
+        table.df = table.df[table.df.iloc[:, 2].str.strip() != '']
+
         # Append the DataFrame to the list
         dataframes.append(table.df)
-
-        #print(f"Processed page 1")
 
     for table in tables_page2_onwards:
         if table.df.empty:
@@ -92,13 +146,31 @@ def extract_tables_with_camelot(pdf_path):
         if not end_index.empty:
             table.df = table.df.loc[:end_index.values[0] - 1]
 
-        # Drop the last column (index 5) from the DataFrame
-        #table.df = table.df.drop(table.df.columns[5], axis=1)
+        #Append the year to the "Date" column for non-empty rows
+        table.df[0] = [f"{date}, {year}" if date.strip() else date for date in table.df[0]]
+
+        # Insert new column for Account Numbers
+        table.df.insert(1, "Account Number", "")
+
+        # Append the account number to the "Account Number" column for non-empty rows
+        table.df["Account Number"] = [f"{account_number}" if description.strip() else description for description in table.df[1]]
+
+        # Loop through the DataFrame starting from the second row
+        for i in range(1, len(table.df)):
+            # Check if the description is not empty for the current row
+            if table.df.iloc[i, 2].strip():
+                # Check if both withdrawals and deposits are empty for the current row
+                if table.df.iloc[i, 3] == '' and table.df.iloc[i, 4] == '':
+                    # Concatenate the description with the next row's description
+                    table.df.iloc[i+1, 2] = table.df.iloc[i, 2] + ' | ' + table.df.iloc[i+1, 2]
+                    # Clear out the current row's description
+                    table.df.iloc[i, 2] = ''
+
+        # Drop rows where the description is empty
+        table.df = table.df[table.df.iloc[:, 2].str.strip() != '']
 
         # Append the DataFrame to the list
         dataframes.append(table.df)
-
-        #print(f"Processed page 2+")
 
     return dataframes
 
@@ -129,24 +201,23 @@ def process_pdfs():
         # Replace empty strings with NaN
         combined_data = combined_data.replace('', np.nan)
         # Drop rows that are completely empty in all columns
-        combined_data = combined_data.dropna(how='all')
+        #combined_data = combined_data.dropna(how='all')
         
         # Remove rows containing headers
-        headers_to_exclude = ["Date", "Description", "Withdrawals ($)", "Deposits ($)", "Balance ($)"]
+        headers_to_exclude = ["Date", re.escape(".*"), "Description", "Withdrawals ($)", "Deposits ($)", "Balance ($)"]
         for header in headers_to_exclude:
             combined_data = combined_data[~combined_data.apply(lambda row: header in " ".join(row.astype(str)), axis=1)]
 
         # Set the DataFrame columns using the headers from the first page
-        combined_data.columns = headers_to_include
-        #combined_data.columns = headers_to_include + ["Extra Date"] # Use when the data is getting shifted from multiple PDFs
+        #combined_data.columns = headers_to_include
+        combined_data.columns = ["Date", "Account #", "Description", "Withdrawals ($)", "Deposits ($)", "Balance ($)"]
 
         # Forward-fill missing dates in the "Date" column
-        combined_data['Date'].fillna(method='ffill', inplace=True)
-
-        #print(combined_data)
+        combined_data["Date"].fillna(method='ffill', inplace=True)
 
         csv_path = os.path.join(PDF_DIR, f"{CSV_FILE}.csv")
         combined_data.to_csv(csv_path, index=False)
+        print(combined_data)
         print(f"Data saved to {csv_path}")
     else:
         print("No data extracted from any PDFs.")
